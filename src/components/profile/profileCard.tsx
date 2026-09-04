119eed04387ebb196394bcc52032f3e17a68c5cc
Angelo Manlangit <manlangitangelomh@gmail.com>
2026-09-04 21:33:10 +0800

    Slight profile card fixes

--- src/components/profile/profileCard.tsx
+++ src/components/profile/profileCard.tsx
@@ -107,6 +107,7 @@ const styles = StyleSheet.create({
     imageBackgroundWrapper: {
         width: '100%',
         height: '100%',
+        paddingVertical: 10
     },
     mainContent: {
         flexDirection: 'row',
